c286cd50f3531c546d66469579031d147fafb9ff
Lin Cheng Yi <66247367+kevin19cool@users.noreply.github.com>
2024-12-11 18:46:08 +0800

    Update app.py
    tempt

--- app.py
+++ app.py
@@ -21,7 +21,10 @@ LINE_CHANNEL_SECRET = "14200733689e76984c5cf98449663211"
 line_bot_api = LineBotApi(LINE_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(LINE_CHANNEL_SECRET)
 
-
+def split_content(content, max_length=500):
+    # 將文章內容分割成不超過指定字數的段落
+    return [content[i:i+max_length] for i in range(0, len(content), max_length)]
+    
 def fetch_full_article_content(article_url):
     # 抓取文章頁面
     response = requests.get(article_url)
@@ -64,20 +67,29 @@ def handle_message(event):
         if not full_content_2:
             reply_message = "未找到文章！"
         else:
-            reply_message ="文章名稱:"
-            reply_message =random_article["title"]
-            reply_message ="文章連結:"
-            reply_message =random_article["news_link"]
-            #reply_message ="文章內容:"
-            #reply_message =full_content_2
+            reply_name ="文章名稱:"+random_article["title"]
+            reply_link ="文章連結:"+random_article["news_link"]
+            reply_content ="文章內容:"
+            # 分割內容
+            split_contents = split_content(full_content_2)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text=reply_name)
+                TextSendMessage(text=reply_link)
+                        )
+             for part in split_contents:
+                line_bot_api.reply_message(
+                    event.reply_token,
+                    TextSendMessage(text=part),
+                        )
     else:
         reply_message = "請輸入 'news' 或 '新聞' 來隨機獲取一篇文章。"
-
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=reply_message),
+        )
     # 回應用戶訊息
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=reply_message)
-    )
+    
 
     
 if __name__ == "__main__":
